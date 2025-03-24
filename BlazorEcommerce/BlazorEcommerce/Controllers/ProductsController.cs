2ad43f9488ec3144e766828985008bd45fb3e230
Bryan Dellinger <bryan.dellinger.civ@armywarcollege.edu>
2025-03-24 14:52:33 -0400

    initial commit

---
+++ BlazorEcommerce/BlazorEcommerce/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace BlazorEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ProductsController(DataContext context)
+        {
+            _context = context; 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts() => Ok(await _context.Products.ToListAsync());
+
+
+    }
+}
---
+++ BlazorEcommerce/BlazorEcommerce/Program.cs
@@ -0,0 +1,59 @@
+global using Domain;
+using BlazorEcommerce.Client.Pages;
+using BlazorEcommerce.Components;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddControllers();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseSwagger();
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+app.MapControllers();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorEcommerce.Client._Imports).Assembly);
+
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<DataContext>();
+await context.Database.MigrateAsync();
+await Seed.SeedData(context);
+
+app.Run();
